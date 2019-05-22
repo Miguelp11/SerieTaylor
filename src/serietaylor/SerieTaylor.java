9cd9645b7af5f1f45b802c2aa0ebbbcc1547d8b3
miguel <migulxpert94@hotmail.com>
2019-05-21 22:50:27 -0500

    #3. Desarrollo de la formula para resolver una funcion senoidal por el metodo de taylor utilizando el metodo factorial creado anteriormente.

--- src/serietaylor/SerieTaylor.java
+++ src/serietaylor/SerieTaylor.java
@@ -4,14 +4,22 @@ package serietaylor;
  * @author Miguel Ponce
  */
 public class SerieTaylor {
-
+/*
+    Este programa resuelve una funcion trigonometrica senoidal, empleando una
+    serie de Taylor, evaluada en la decimo tercera derivada, el valor de "x" es 
+    estatico en el codigo de programacion y el resultado es mostrado en consola 
+    con un valor decimal representado en radianes.
+    */
     public static void main(String[] args) {
         Metodos metodo = new Metodos();
+        int x= 1; 
+        int signo=1;
+        double resultado=0,calculo;
             for(int i=1;i<=13;i+=2){
-            System.out.println("factorial de "+i+" "+metodo.factorial(i));
+            calculo= Math.pow (x, i) / (metodo.factorial(i) * (signo));
+            signo = signo * (-1);
+            resultado = resultado+calculo;
             }
-            for(int j=1;j<=7;j+=2){
-            System.out.println("el exponente de "+j+" es " +metodo.exponente(j));
-            }
+            System.out.println("f("+x+") = "+resultado);
     }    
 }
